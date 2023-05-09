ce0b1cc8c7a32c79fd131ea9ecf98231f62dea7c
Shaniqua Coston <shaniquacoston@pursuit.org>
2023-05-09 00:28:50 -0400

    added values to the sneakers_dev database

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS sneakers_dev;
+CREATE DATABASE sneakers_dev;
+
+\c sneakers_dev;
+
+CREATE TABLE sneakers (
+   id SERIAL PRIMARY KEY,
+   name TEXT NOT NULL,
+   brand TEXT NOT NULL, 
+   release_date DATE,
+   price INT, 
+   rating NUMERIC,
+   CHECK (rating >= 0 AND rating <= 5),
+   is_favorite BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE images (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    url TEXT NOT NULL,
+    sneaker_ID INTEGER NOT NULL REFERENCES sneakers (id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE links (
+    id SERIAL PRIMARY KEY,
+    store_name TEXT NOT NULL,
+    url TEXT NOT NULL,
+    sneaker_ID INTEGER NOT NULL REFERENCES sneakers (id)
+); 
